bf0e4f510aa1cc30e2672d9d6841286c96bba99c
Mihai Rusu <mrusu@2-B-9.42heilbronn.de>
2025-02-01 16:14:25 +0100

    info print color, asci art, print error exit can not exit if not necesarly

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -11,53 +11,118 @@ Server::~Server()
         close(serverSocket_);
 }
 
-void Server::printErrorExit(const std::string &msg)
+void Server::printErrorExit(std::string const& msg, bool exitP)
 {
-    std::cerr << "Error: " << msg << std::endl;
-    exit(1);
+    std::cerr << RED "Error: " RESET << msg << std::endl;
+    if (exitP)
+        exit(1);
+}
+
+void Server::printInfo(messageType type, const std::string &msg)
+{
+    // Get current time
+    std::time_t now = std::time(nullptr);
+    std::tm *tm_now = std::localtime(&now);
+    std::ostringstream oss;
+    oss << std::put_time(tm_now, "%H:%M:%S");
+
+    std::string typeStr;
+    std::string color;
+
+    switch (type)
+    {
+        case INFO:
+            typeStr = "INFO";
+            color = BLUE;
+            break;
+        case WARNING:
+            typeStr = "WARNING";
+            color = YELLOW;
+            break;
+        case CONNECTION:
+            typeStr = "CONNECTION";
+            color = GREEN;
+            break;
+        case DISCONNECTION:
+            typeStr = "DISCONNECTION";
+            color = RED;
+            break;
+        case PING:
+            typeStr = "PING";
+            color = GREEN;
+            break;
+        case PONG:
+            typeStr = "PONG";
+            color = GREEN;
+            break;
+        case CLIENT:
+            typeStr = "CLIENT";
+            color = BLUE;
+            break;
+        case SERVER:
+            typeStr = "SERVER";
+            color = BLUE;
+            break;
+        case TIMEOUT:
+            typeStr = "TIMEOUT";
+            color = RED;
+            break;
+        case SUCCESS:
+            typeStr = "SUCCESS";
+            color = GREEN;
+            break;
+        default:
+            typeStr = "UNKNOWN";
+            color = RESET;
+            break;
+    }
+    std::cout << color << "[" << typeStr << "]"  << GRAY << " [" << oss.str()<< "] "
+        << RESET << msg << std::endl;
 }
 
 void Server::setup()
 {
     serverSocket_ = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket_ < 0)
-        printErrorExit("Error establishing socket...");
+        printErrorExit("Establishing socket...");
     
-
-    std::cout << "Socket server has been created..." << std::endl;
+    printInfo(INFO, "Socket server has been created.");
 
     serverAddr_.sin_family = AF_INET;
     serverAddr_.sin_addr.s_addr = INADDR_ANY;
     serverAddr_.sin_port = htons(port_);
 
     if (bind(serverSocket_, (struct sockaddr *)&serverAddr_, sizeof(serverAddr_)) < 0)
-        printErrorExit("Error binding socket...");
+        printErrorExit("Error binding socket.", true);
     
-    std::cout << "Binding success..." << std::endl;
+    printInfo(INFO, "Binding success...");
 
     if (listen(serverSocket_, 5) < 0)
-        printErrorExit("Error listening...");
-    
-    std::cout << "__        _______ _     ____ ___  __  __ _____ \n";
-    std::cout << "\\ \\      / / ____| |   / ___/ _ \\|  \\/  | ____|\n";
-    std::cout << " \\ \\ /\\ / /|  _| | |  | |  | | | | |\\/| |  _|  \n";
-    std::cout << "  \\ V  V / | |___| |__| |__| |_| | |  | | |___ \n";
-    std::cout << "   \\_/\\_/  |_____|_____\\____\\___/|_|  |_|_____|\n\n";
-
-    std::cout << "Listening on port " << port_ << "..." << std::endl;
+        printErrorExit("Error listening...", true);
 }
 
 
 void Server::start() 
 {
+    std::cout << GREEN R"(░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░█░█░█▀▀░█░░░█▀▀░█▀█░█▄█░█▀▀░░░░░░░░░░░░░        
+░░░░░░░░░░░░░░░░░█▄█░█▀▀░█░░░█░░░█░█░█░█░█▀▀░░░░░░░░░░░░░       
+░░░░░░░░░░░░░░░░░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░░░░░░░░░░         
+░█▀▀░█▀▀░█▀▄░█░█░█▀▀░█▀▄░░░█▀▀░▀█▀░█▀█░█▀▄░▀█▀░█▀▀░█▀▄░░░
+░▀▀█░█▀▀░█▀▄░▀▄▀░█▀▀░█▀▄░░░▀▀█░░█░░█▀█░█▀▄░░█░░█▀▀░█░█░░░
+░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀░░░▀▀▀░░▀░░▀░▀░▀░▀░░▀░░▀▀▀░▀▀░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░)" << std::endl;
+    printInfo(INFO, "Server listening on ip:port " + std::to_string(port_));
+    printInfo(INFO, "Waiting for conections...");
     fd_set readfds;
-    while (true) {
+    while (true) // or sometign lieka bool 
+    {
         FD_ZERO(&readfds);
         FD_SET(serverSocket_, &readfds);
-
         select(serverSocket_ + 1, &readfds, NULL, NULL, NULL);
 
-        if (FD_ISSET(serverSocket_, &readfds)) {
+        if (FD_ISSET(serverSocket_, &readfds))
+        {
             acceptClient();
         }
     }
@@ -66,23 +131,26 @@ void Server::start()
 void Server::acceptClient() 
 {
     int clientSocket = accept(serverSocket_, NULL, NULL);
-    if (clientSocket < 0) {
-        std::cerr << "Accept failed" << std::endl;
+    if (clientSocket < 0)
+    {
+        printErrorExit("Accept failed!", false);
         return;
     }
     // clientSocket.push_back(clientSocket);
-    std::cout << "New client connected" << std::endl;
+    printInfo(INFO, "New client connected.");
 }
 
 void Server::handleClient(int clientSocket) 
 {
     char buffer[1024];
     int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesRead <= 0) {
+    if (bytesRead <= 0)
+    {
         close(clientSocket);
+        printInfo(INFO, "Client disconnected");
         return;
     }
     // Basic message handling
     buffer[bytesRead] = '\0';
-    std::cout << "Received: " << buffer << std::endl;
+    printInfo(CLIENT, std::string("Received: ") + buffer);
 }
--- srcs/Server.hpp
+++ srcs/Server.hpp
@@ -7,8 +7,34 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <cstring>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+#include <arpa/inet.h>
 
 #include "Client.hpp"
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define GRAY "\033[90m"
+
+
+enum messageType
+{
+    INFO,
+    WARNING,
+    CONNECTION,
+    DISCONNECTION,
+    PING,
+    PONG,
+    CLIENT,
+    SERVER,
+    TIMEOUT,
+    SUCCESS,
+};
 
 class Server
 {
@@ -18,7 +44,8 @@ class Server
 
         void       setup();
         void       start();
-        void       printErrorExit(const std::string &msg);
+        void       printErrorExit(std::string const& msg, bool exitP = false);
+        void       printInfo(messageType type, std::string const& msg);
         void       acceptClient();
         void       handleClient(int clientSocket);
         // void       executeCommand(int clientSocket, const std::string &command);
@@ -29,4 +56,5 @@ class Server
         int         serverSocket_;             // Server socket file descriptor
         struct sockaddr_in serverAddr_;        // Server address
         std::vector<int> clientSockets_;       // Client socket file descriptors
+        // std::string getIPAddress();
 };
